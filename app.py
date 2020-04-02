e2fe92d0e21731a7dddea638edadc0fd0299150d
minhphan88 <minh.phan88@outlook.com>
2020-04-02 15:58:34 +0100

    cal 1.0

--- app.py
+++ app.py
@@ -15,4 +15,21 @@ def page():
 
 @app.route('/cal')
 def cal():
-    return my_function
+    a = request.args.get('number1', '0')
+    operator = request.args.get('operator', '+')
+    b = request.args.get('number2', '0')
+    # validate the input data
+    m = re.match(r'^\-?\d*[.]?\d*$', a)
+    n = re.match(r'^\-?\d*[.]?\d*$', b)
+
+    if m is None or n is None or operator not in '+-*/':
+        return jsonify(result='Error!')
+
+    if operator == '/':
+        result = eval(a + operator + str(float(b)))
+    else:
+        result = eval(a + operator + b)
+    return jsonify(result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
