2230dafe7d86e799a44054316097bc9bdcd2982e
minhphan88 <minh.phan88@outlook.com>
2020-04-02 10:33:15 +0100

    print

--- facilities_main.py
+++ facilities_main.py
@@ -1,16 +1,1 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-class UserIn(BaseModel):
-    username: str
-    password: str
-    email: EmailStr
-    full_name: str = None
-
-
-# Don't do this in production!
-@app.post("/user/", response_model=UserIn)
-async def create_user(*, user: UserIn):
-    return user
+print ("page 1")
