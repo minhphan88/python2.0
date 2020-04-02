7bfb96405f7d28824bc2998f7bf2343445ba4fd0
minhphan88 <minh.phan88@outlook.com>
2020-04-02 19:01:04 +0100

    cal 1.3

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
-def my_function():
-    print("Hello from a function")
+
 
 @app.route('/')
 def index():
@@ -14,14 +13,6 @@ def index():
 def page():
     return 'This is an different page'
 
-@app.route('/cal', methods= methods=['POST'])
-def cal(height):
-    try:
-        height=json.loads(heightdata.body)
-        weight=str(height*10)
-        return weight
-    except ValueError as e:
-        return 0
 
 
 if __name__ == '__main__':
