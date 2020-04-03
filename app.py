886c4fbe69164d72341d86eba823ad2fb37254d1
minhphan88 <minh.phan88@outlook.com>
2020-04-03 15:48:21 +0100

    no query

--- app.py
+++ app.py
@@ -12,10 +12,6 @@ def index():
 def page():
     return 'This is an different page'
 
-@app.route('/query_example'):
-def query_example():
-    language=request.args.get('language')
-    return '<h1> The language is : {}</h1>'.format (language)
 
 if __name__ == '__main__':
     app.run(debug=True)
